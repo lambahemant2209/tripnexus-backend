9984e6c87b3ec8a6bdcaba66c4438a70504a82dc
lambahemant2209 <lambahemant987@gmail.com>
2025-11-30 23:01:03 +0530

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    passwordHash: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+// THIS is the important line: export one Mongoose model
+module.exports = mongoose.model("User", userSchema);
